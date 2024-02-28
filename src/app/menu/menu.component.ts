8447ffb1e62543caf0e2b64e5861a179f260c832
crecioa <crecio.santos@advmpm.com.br>
2024-02-28 15:31:05 -0300

    teste

---
+++ src/app/menu/menu.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
+@Component({
+  selector: 'app-menu',
+  templateUrl: './menu.component.html',
+  styleUrls: ['./menu.component.css']
+})
+export class MenuComponent {
+
+}
